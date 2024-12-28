800a16f6ed46b353f250a43474f23dabd1817ddf
khsaad04 <khsaad0404@gmail.com>
2024-12-28 16:35:21 +0600

    refactor(nvim): Remove redundant configurations

--- dotfiles/.config/nvim/lua/plugins/treesitter.lua
+++ dotfiles/.config/nvim/lua/plugins/treesitter.lua
@@ -4,25 +4,11 @@ return {
     build = ":TSUpdate",
     config = function()
         require("nvim-treesitter.configs").setup({
-            -- Add languages to be installed here that you want installed for treesitter
-            ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
-            auto_install = true,
-
             highlight = {
                 enable = true,
-                additional_vim_regex_highlighting = false,
             },
             indent = {
                 enable = true,
-            },
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-                    init_selection = "<leader>ss", -- set to `false` to disable one of the mappings
-                    node_incremental = "<leader>si",
-                    node_decremental = "<leader>sd",
-                    scope_incremental = "<leader>sc",
-                },
             },
         })
     end,
