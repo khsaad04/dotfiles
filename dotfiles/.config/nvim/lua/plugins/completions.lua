d421b25d0c4aac0d1bcbabfaab6c979c9ddb4f74
khsaad04 <khsaad0404@gmail.com>
2024-12-30 15:28:53 +0600

    refactor(nvim): Simplify some configurations

--- dotfiles/.config/nvim/lua/plugins/completions.lua
+++ dotfiles/.config/nvim/lua/plugins/completions.lua
@@ -27,37 +27,9 @@ return {
                 end,
             },
             mapping = cmp.mapping.preset.insert({
-                ["<CR>"] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        if luasnip.expandable() then
-                            luasnip.expand()
-                        else
-                            cmp.confirm({
-                                select = true,
-                            })
-                        end
-                    else
-                        fallback()
-                    end
-                end),
-                ["<Tab>"] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.select_next_item()
-                    elseif luasnip.locally_jumpable(1) then
-                        luasnip.jump(1)
-                    else
-                        fallback()
-                    end
-                end, { "i", "s" }),
-                ["<S-Tab>"] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.select_prev_item()
-                    elseif luasnip.locally_jumpable(-1) then
-                        luasnip.jump(-1)
-                    else
-                        fallback()
-                    end
-                end, { "i", "s" }),
+                ["<CR>"] = cmp.mapping.confirm({ select = true }),
+                ["<Tab>"] = cmp.mapping.select_next_item(),
+                ["<S-Tab>"] = cmp.mapping.select_prev_item(),
             }),
             sources = cmp.config.sources({
                 { name = "nvim_lsp" },
--- dotfiles/.config/nvim/lua/plugins/misc.lua
+++ dotfiles/.config/nvim/lua/plugins/misc.lua
@@ -1,16 +1,3 @@
 return {
     { "tpope/vim-sleuth", event = "VeryLazy" }, -- Automatically detect and change indent size
-    {
-        "lukas-reineke/indent-blankline.nvim", -- Show indents
-        main = "ibl",
-        event = "VeryLazy",
-        opts = {
-            indent = {
-                char = "┊",
-            },
-            scope = {
-                enabled = false,
-            },
-        },
-    },
 }
