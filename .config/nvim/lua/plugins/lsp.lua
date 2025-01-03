4b94feeb1af6c55d53785dbe673d93918845e656
khsaad04 <khsaad0404@gmail.com>
2025-01-03 21:42:29 +0600

    refactor(nvim): lsp config

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -2,26 +2,20 @@ return {
     "neovim/nvim-lspconfig",
     lazy = false,
     config = function()
-        local lspconfig = require("lspconfig")
+        -- Setup lsp server configs
         local capabilities = vim.lsp.protocol.make_client_capabilities()
         capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
-        lspconfig.util.default_config = vim.tbl_extend("force", lspconfig.util.default_config, {
-            on_attach = function(_, bufnr)
-                vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "LSP: [R]e[n]ame" })
-                vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "LSP: [C]ode [A]ction" })
-                vim.diagnostic.enable(false)
-            end,
-            capabilities = capabilities,
-        })
-
-        -- Setup lsp server configs
         for server, config in pairs(require("config.lsp_config")) do
-            lspconfig[server].setup(config)
+            require("lspconfig")[server].setup(vim.tbl_extend("force", config, {
+                on_attach = function(_, _)
+                    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "LSP: [R]e[n]ame" })
+                    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "LSP: [C]ode [A]ction" })
+                end,
+                capabilities = capabilities,
+            }))
         end
 
         -- Diagnostics
-        vim.keymap.set("n", "<leader>q", require("telescope.builtin").diagnostics, { desc = "[Q]uickfix" })
-        vim.keymap.set("n", "<space>e", vim.diagnostic.open_float, { desc = "Open diagnostic float" })
         local signs = {
             { name = "DiagnosticSignError", text = "" },
             { name = "DiagnosticSignWarn", text = "" },
@@ -37,5 +31,14 @@ return {
             },
             severity_sort = true,
         })
+        vim.keymap.set("n", "<leader>q", require("telescope.builtin").diagnostics, { desc = "[Q]uickfix" })
+        vim.keymap.set("n", "<space>e", vim.diagnostic.open_float, { desc = "Open diagnostic float" })
+        vim.keymap.set(
+            "n",
+            "<leader>td",
+            "<cmd>lua vim.diagnostic.enable(not vim.diagnostic.is_enabled())<CR>",
+            { desc = "[T]oggle [D]iagnostics" }
+        )
+        vim.diagnostic.enable(false)
     end,
 }
