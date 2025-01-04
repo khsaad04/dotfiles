13558cc5067dc365e3b13415123ad79e211555ac
khsaad04 <khsaad0404@gmail.com>
2025-01-04 20:16:31 +0600

    feat(nvim): Merge trivial plugins into one misc file

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -1,3 +1,32 @@
 return {
-    { "tpope/vim-sleuth", event = "VeryLazy" }, -- Automatically detect and change indent size
+    { -- Automatically detect and change indent size
+        "tpope/vim-sleuth",
+        event = "VeryLazy",
+    },
+    { -- Show Git signs on the signcolumn
+        "lewis6991/gitsigns.nvim",
+        event = "VeryLazy",
+        config = function()
+            require("gitsigns").setup()
+        end,
+    },
+    { -- Git client
+        "tpope/vim-fugitive",
+        event = "VeryLazy",
+    },
+    { -- Multi cursor support
+        "mg979/vim-visual-multi",
+        event = "VeryLazy",
+        keys = {
+            {
+                "<leader>vmm",
+                "<Plug>(VM-Toggle-Mappings)",
+                mode = "n",
+                desc = "[V]isual-[M]ulti Toggle [M]appings",
+            },
+        },
+        init = function()
+            vim.g.VM_add_cursor_at_pos_no_mappings = 1
+        end,
+    },
 }
