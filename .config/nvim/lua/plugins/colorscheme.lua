5e5af61e5bd6c928019c3b712e6f2558f511483f
khsaad04 <khsaad0404@gmail.com>
2025-01-15 11:37:04 +0600

    refactor: Format nvim-colorscheme.lua

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,16 +1,16 @@
 return {
-	"ellisonleao/gruvbox.nvim",
-	priority = 1000,
-	config = function()
-		require("gruvbox").setup({
-			contrast = "hard",
-			palette_overrides = {
-				dark0_hard = "#13140c",
-				dark1 = "#1c1c14",
-				dark2 = "#202018",
-			},
-		})
-		vim.opt.background = "dark"
-		vim.cmd.colorscheme("gruvbox")
-	end,
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    config = function()
+        require("gruvbox").setup({
+            contrast = "hard",
+            palette_overrides = {
+                dark0_hard = "#13140c",
+                dark1 = "#1c1c14",
+                dark2 = "#202018",
+            },
+        })
+        vim.opt.background = "dark"
+        vim.cmd.colorscheme("gruvbox")
+    end,
 }
--- templates/nvim-colorscheme.lua
+++ templates/nvim-colorscheme.lua
@@ -1,16 +1,16 @@
 return {
-	"ellisonleao/gruvbox.nvim",
-	priority = 1000,
-	config = function()
-		require("gruvbox").setup({
-			contrast = "hard",
-			palette_overrides = {
-				dark0_hard = "#{{surface}}",
-				dark1 = "#{{surface_container_low}}",
-				dark2 = "#{{surface_container}}",
-			},
-		})
-		vim.opt.background = "dark"
-		vim.cmd.colorscheme("gruvbox")
-	end,
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    config = function()
+        require("gruvbox").setup({
+            contrast = "hard",
+            palette_overrides = {
+                dark0_hard = "#{{surface}}",
+                dark1 = "#{{surface_container_low}}",
+                dark2 = "#{{surface_container}}",
+            },
+        })
+        vim.opt.background = "dark"
+        vim.cmd.colorscheme("gruvbox")
+    end,
 }
