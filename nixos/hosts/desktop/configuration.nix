98ace2c5ef1855cb494258a61712ef22f3a16510
khsaad04 <khsaad0404@gmail.com>
2025-01-15 11:37:20 +0600

    refactor: Install all the pkgs system-wide

--- nixos/hosts/desktop/configuration.nix
+++ nixos/hosts/desktop/configuration.nix
@@ -41,6 +41,62 @@
       EDITOR = "nvim";
       VISUAL = "nvim";
     };
+    systemPackages = [
+      # theming
+      pkgs.adw-gtk3
+      pkgs.adwaita-icon-theme
+      pkgs.qt6Packages.qtstyleplugin-kvantum
+      pkgs.qt6Packages.qt6ct
+      pkgs.libsForQt5.qtstyleplugin-kvantum
+      pkgs.libsForQt5.qt5ct
+
+      # neovim
+      pkgs.neovim
+      pkgs.luajitPackages.jsregexp
+      pkgs.cmake
+      pkgs.unzip
+      pkgs.tree-sitter
+      pkgs.ripgrep
+      pkgs.lua-language-server
+      pkgs.nil
+      pkgs.stylua
+      pkgs.prettierd
+
+      # devel tools
+      pkgs.wget
+      pkgs.file
+      pkgs.fd
+      pkgs.ripgrep
+      pkgs.cargo
+      pkgs.gcc
+      pkgs.python3
+      pkgs.git
+
+      # console
+      pkgs.foot
+      pkgs.starship
+      pkgs.tmux
+      pkgs.tmux-sessionizer
+
+      # misc
+      pkgs.firefox
+      pkgs.xdg-utils
+      pkgs.btop
+      pkgs.wl-clipboard
+      pkgs.wl-clip-persist
+      pkgs.qbittorrent
+      pkgs.pavucontrol
+      pkgs.yazi
+      pkgs.zathura
+      pkgs.obs-studio
+      pkgs.mpv
+      pkgs.feh
+      pkgs.udiskie
+      inputs.self.packages.${pkgs.system}.formatter
+      inputs.self.packages.${pkgs.system}.changevolume
+      inputs.self.packages.${pkgs.system}.powermenu
+      inputs.dotman.packages.${pkgs.system}.default
+    ];
   };
 
   programs.dconf.profiles.user.databases = [
@@ -62,62 +118,6 @@
         "networkmanager"
         "wheel"
         "adbusers"
-      ];
-      packages = [
-        # theming
-        pkgs.adw-gtk3
-        pkgs.adwaita-icon-theme
-        pkgs.qt6Packages.qtstyleplugin-kvantum
-        pkgs.qt6Packages.qt6ct
-        pkgs.libsForQt5.qtstyleplugin-kvantum
-        pkgs.libsForQt5.qt5ct
-
-        # neovim
-        pkgs.neovim
-        pkgs.luajitPackages.jsregexp
-        pkgs.cmake
-        pkgs.unzip
-        pkgs.tree-sitter
-        pkgs.ripgrep
-        pkgs.lua-language-server
-        pkgs.nil
-        pkgs.stylua
-        pkgs.prettierd
-
-        # devel tools
-        pkgs.wget
-        pkgs.file
-        pkgs.fd
-        pkgs.ripgrep
-        pkgs.cargo
-        pkgs.gcc
-        pkgs.python3
-        pkgs.git
-
-        # console
-        pkgs.foot
-        pkgs.starship
-        pkgs.tmux
-        pkgs.tmux-sessionizer
-
-        # misc
-        pkgs.firefox
-        pkgs.xdg-utils
-        pkgs.btop
-        pkgs.wl-clipboard
-        pkgs.wl-clip-persist
-        pkgs.qbittorrent
-        pkgs.pavucontrol
-        pkgs.yazi
-        pkgs.zathura
-        pkgs.obs-studio
-        pkgs.mpv
-        pkgs.feh
-        pkgs.udiskie
-        inputs.self.packages.${pkgs.system}.formatter
-        inputs.self.packages.${pkgs.system}.changevolume
-        inputs.self.packages.${pkgs.system}.powermenu
-        inputs.dotman.packages.${pkgs.system}.default
       ];
     };
   };
