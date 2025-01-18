2ad52946a6f350aca4c878c753ac994e22bc6b8a
khsaad04 <khsaad0404@gmail.com>
2025-01-18 12:59:02 +0600

    feat: Update dotman

--- templates/nvim-colorscheme.lua
+++ templates/nvim-colorscheme.lua
@@ -1,25 +1,25 @@
 return {
-	"ellisonleao/gruvbox.nvim",
-	priority = 1000,
-	config = function()
-		if "{{theme}}" == "dark" then
-			require("gruvbox").setup({
-				palette_overrides = {
-					dark0 = "#{{surface}}",
-					dark1 = "#{{surface_container_low}}",
-					dark2 = "#{{surface_container}}",
-				},
-			})
-		else
-			require("gruvbox").setup({
-				palette_overrides = {
-					light0 = "#{{surface}}",
-					light1 = "#{{surface_container_low}}",
-					light2 = "#{{surface_container}}",
-				},
-			})
-		end
-		vim.opt.background = "{{theme}}"
-		vim.cmd.colorscheme("gruvbox")
-	end,
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    config = function()
+        if "{{theme}}" == "dark" then
+            require("gruvbox").setup({
+                palette_overrides = {
+                    dark0 = "#{{surface}}",
+                    dark1 = "#{{surface_container_low}}",
+                    dark2 = "#{{surface_container}}",
+                },
+            })
+        else
+            require("gruvbox").setup({
+                palette_overrides = {
+                    light0 = "#{{surface}}",
+                    light1 = "#{{surface_container_low}}",
+                    light2 = "#{{surface_container}}",
+                },
+            })
+        end
+        vim.opt.background = "{{theme}}"
+        vim.cmd.colorscheme("gruvbox")
+    end,
 }
