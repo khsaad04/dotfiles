e05dbde41f2fef463e35472f60b9ab3dcf51c867
khsaad04 <khsaad0404@gmail.com>
2025-01-18 12:44:06 +0600

    fix: adjust colors to look better in light theme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,16 +1,25 @@
 return {
-    "ellisonleao/gruvbox.nvim",
-    priority = 1000,
-    config = function()
-        require("gruvbox").setup({
-            contrast = "hard",
-            palette_overrides = {
-                dark0_hard = "#13140c",
-                dark1 = "#1c1c14",
-                dark2 = "#202018",
-            },
-        })
-        vim.opt.background = "dark"
-        vim.cmd.colorscheme("gruvbox")
-    end,
+	"ellisonleao/gruvbox.nvim",
+	priority = 1000,
+	config = function()
+		if "dark" == "dark" then
+			require("gruvbox").setup({
+				palette_overrides = {
+					dark0 = "#13140c",
+					dark1 = "#1c1c14",
+					dark2 = "#202018",
+				},
+			})
+		else
+			require("gruvbox").setup({
+				palette_overrides = {
+					light0 = "#13140c",
+					light1 = "#1c1c14",
+					light2 = "#202018",
+				},
+			})
+		end
+		vim.opt.background = "dark"
+		vim.cmd.colorscheme("gruvbox")
+	end,
 }
--- templates/nvim-colorscheme.lua
+++ templates/nvim-colorscheme.lua
@@ -1,16 +1,25 @@
 return {
-    "ellisonleao/gruvbox.nvim",
-    priority = 1000,
-    config = function()
-        require("gruvbox").setup({
-            contrast = "hard",
-            palette_overrides = {
-                dark0_hard = "#{{surface}}",
-                dark1 = "#{{surface_container_low}}",
-                dark2 = "#{{surface_container}}",
-            },
-        })
-        vim.opt.background = "dark"
-        vim.cmd.colorscheme("gruvbox")
-    end,
+	"ellisonleao/gruvbox.nvim",
+	priority = 1000,
+	config = function()
+		if "{{theme}}" == "dark" then
+			require("gruvbox").setup({
+				palette_overrides = {
+					dark0 = "#{{surface}}",
+					dark1 = "#{{surface_container_low}}",
+					dark2 = "#{{surface_container}}",
+				},
+			})
+		else
+			require("gruvbox").setup({
+				palette_overrides = {
+					light0 = "#{{surface}}",
+					light1 = "#{{surface_container_low}}",
+					light2 = "#{{surface_container}}",
+				},
+			})
+		end
+		vim.opt.background = "{{theme}}"
+		vim.cmd.colorscheme("gruvbox")
+	end,
 }
