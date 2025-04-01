21763e194c8df46ee34a418074f480ad0db28cc9
khsaad04 <khsaad0404@gmail.com>
2025-04-01 22:41:50 +0600

    refactor(nvim): refactor the colorscheme config

--- configs/.config/nvim/lua/plugins/colorscheme.lua
+++ configs/.config/nvim/lua/plugins/colorscheme.lua
@@ -2,32 +2,28 @@ return {
     "ellisonleao/gruvbox.nvim",
     priority = 1000,
     config = function()
-        if "dark" == "dark" then
-            require("gruvbox").setup({
-                palette_overrides = {
-                    dark0 = "#13140c",
-                    dark1 = "#1c1c14",
-                    dark2 = "#202018",
-                },
-                overrides = {
-                    StatusLine = { bg = "#202018", fg = "#e5e3d6" },
-                    StatusLineNC = { bg = "#202018", fg = "#e5e3d6" }
-                }
-            })
-        else
-            require("gruvbox").setup({
-                palette_overrides = {
-                    light0 = "#13140c",
-                    light1 = "#1c1c14",
-                    light2 = "#202018",
-                },
-                overrides = {
-                    StatusLine = { bg = "#202018", fg = "#e5e3d6" },
-                    StatusLineNC = { bg = "#202018", fg = "#e5e3d6" }
-                }
-            })
-        end
         vim.opt.background = "dark"
+        local palette
+        if "dark" == "dark" then
+            palette = {
+                dark0 = "#13140c",
+                dark1 = "#1c1c14",
+                dark2 = "#202018",
+            }
+        else
+            palette = {
+                light0 = "#13140c",
+                light1 = "#1c1c14",
+                light2 = "#202018",
+            }
+        end
+        require("gruvbox").setup({
+            palette_overrides = palette,
+            overrides = {
+                StatusLine = { bg = "#202018", fg = "#e5e3d6" },
+                StatusLineNC = { bg = "#202018", fg = "#e5e3d6" },
+            },
+        })
         vim.cmd.colorscheme("gruvbox")
     end,
 }
--- templates/nvim-colorscheme.lua
+++ templates/nvim-colorscheme.lua
@@ -2,32 +2,28 @@ return {
     "ellisonleao/gruvbox.nvim",
     priority = 1000,
     config = function()
-        if "{{theme}}" == "dark" then
-            require("gruvbox").setup({
-                palette_overrides = {
-                    dark0 = "#{{surface}}",
-                    dark1 = "#{{surface_container_low}}",
-                    dark2 = "#{{surface_container}}",
-                },
-                overrides = {
-                    StatusLine = { bg = "#{{surface_container}}", fg = "#{{on_surface}}" },
-                    StatusLineNC = { bg = "#{{surface_container}}", fg = "#{{on_surface}}" },
-                },
-            })
-        else
-            require("gruvbox").setup({
-                palette_overrides = {
-                    light0 = "#{{surface}}",
-                    light1 = "#{{surface_container_low}}",
-                    light2 = "#{{surface_container}}",
-                },
-                overrides = {
-                    StatusLine = { bg = "#{{surface_container}}", fg = "#{{on_surface}}" },
-                    StatusLineNC = { bg = "#{{surface_container}}", fg = "#{{on_surface}}" },
-                },
-            })
-        end
         vim.opt.background = "{{theme}}"
+        local palette
+        if "{{theme}}" == "dark" then
+            palette = {
+                dark0 = "#{{surface}}",
+                dark1 = "#{{surface_container_low}}",
+                dark2 = "#{{surface_container}}",
+            }
+        else
+            palette = {
+                light0 = "#{{surface}}",
+                light1 = "#{{surface_container_low}}",
+                light2 = "#{{surface_container}}",
+            }
+        end
+        require("gruvbox").setup({
+            palette_overrides = palette,
+            overrides = {
+                StatusLine = { bg = "#{{surface_container}}", fg = "#{{on_surface}}" },
+                StatusLineNC = { bg = "#{{surface_container}}", fg = "#{{on_surface}}" },
+            },
+        })
         vim.cmd.colorscheme("gruvbox")
     end,
 }
