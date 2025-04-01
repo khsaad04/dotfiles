7189184b6d75cd49f21ddccb26312ef04044ea9e
khsaad04 <khsaad0404@gmail.com>
2025-04-01 19:52:49 +0600

    feat(nvim): Replace nvim-cmp with blink.cmp for completions

--- configs/.config/nvim/lua/plugins/completions.lua
+++ configs/.config/nvim/lua/plugins/completions.lua
@@ -1,56 +1,7 @@
 return {
-    "hrsh7th/nvim-cmp",
+    "saghen/blink.cmp",
     event = { "InsertEnter", "CmdlineEnter" },
-    dependencies = {
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-cmdline",
-        {
-            "L3MON4D3/LuaSnip",
-            dependencies = { "saadparwaiz1/cmp_luasnip", "rafamadriz/friendly-snippets" },
-        },
-    },
-    config = function()
-        local cmp = require("cmp")
-        local luasnip = require("luasnip")
-
-        -- LuaSnip
-        require("luasnip.loaders.from_vscode").lazy_load()
-        luasnip.config.setup({})
-
-        -- nvim-cmp
-        cmp.setup({
-            snippet = {
-                expand = function(args)
-                    luasnip.lsp_expand(args.body)
-                end,
-            },
-            mapping = cmp.mapping.preset.insert({
-                ["<CR>"] = cmp.mapping.confirm({ select = true }),
-                ["<Tab>"] = cmp.mapping.select_next_item(),
-                ["<S-Tab>"] = cmp.mapping.select_prev_item(),
-            }),
-            sources = cmp.config.sources({
-                { name = "nvim_lsp" },
-                { name = "buffer" },
-                { name = "path" },
-                { name = "luasnip" },
-            }),
-        })
-        cmp.setup.cmdline({ "/", "?" }, {
-            mapping = cmp.mapping.preset.cmdline(),
-            sources = {
-                { name = "buffer" },
-            },
-        })
-        cmp.setup.cmdline(":", {
-            mapping = cmp.mapping.preset.cmdline(),
-            sources = cmp.config.sources({
-                { name = "path" },
-                { name = "cmdline" },
-            }),
-            matching = { disallow_symbol_nonprefix_matching = false },
-        })
-    end,
+    dependencies = { "rafamadriz/friendly-snippets" },
+    build = "nix run .#build-plugin",
+    opts = {}
 }
