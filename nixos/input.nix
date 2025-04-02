f1a6e3471b2260fcc40eb621a927cb3cbf9fbc8a
khsaad04 <khsaad0404@gmail.com>
2025-04-02 20:38:33 +0600

    feat: setup interception-caps2esc and remove sway hack

--- nixos/input.nix
+++ nixos/input.nix
@@ -8,4 +8,15 @@
       fcitx5.addons = [ pkgs.fcitx5-openbangla-keyboard ];
     };
   };
+
+  services.interception-tools = {
+    enable = true;
+    plugins = [ pkgs.interception-tools-plugins.caps2esc ];
+    udevmonConfig = ''
+      - JOB: "${pkgs.interception-tools}/bin/intercept -g $DEVNODE | ${pkgs.interception-tools-plugins.caps2esc}/bin/caps2esc | ${pkgs.interception-tools}/bin/uinput -d $DEVNODE"
+        DEVICE:
+          EVENTS:
+            EV_KEY: [KEY_CAPSLOCK, KEY_ESC]
+    '';
+  };
 }
