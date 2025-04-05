3e105728d1c8c8202e17120bc5be2612102a7bcc
khsaad04 <khsaad0404@gmail.com>
2025-04-05 13:31:34 +0600

    feat(nvim): Implement a mini.deps based config [Experimentation]

---
+++ configs/.config/nvim/lua/mini/lsp.lua
@@ -0,0 +1,16 @@
+-- Lsp
+vim.lsp.enable({ "clangd", "luals", "nil_ls", "pylsp", "ruff", "rust-analyzer" })
+
+-- Diagnostics
+vim.diagnostic.config({
+    severity_sort = true,
+    signs = {
+        text = {
+            [vim.diagnostic.severity.ERROR] = "",
+            [vim.diagnostic.severity.WARN] = "",
+            [vim.diagnostic.severity.INFO] = "",
+            [vim.diagnostic.severity.HINT] = "",
+        },
+    },
+})
+vim.diagnostic.enable(false)
---
+++ configs/.config/nvim/lua/mini/options.lua
@@ -0,0 +1,31 @@
+local opt = vim.opt
+
+opt.number = true
+opt.relativenumber = true
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.smartindent = true
+opt.splitbelow = true
+opt.splitright = true
+opt.wrap = false
+opt.scrolloff = 999
+opt.virtualedit = "block"
+opt.clipboard = "unnamedplus"
+opt.undofile = true
+opt.swapfile = false
+opt.updatetime = 250
+opt.timeoutlen = 300
+opt.ignorecase = true
+opt.smartcase = true
+opt.inccommand = "split"
+opt.signcolumn = "yes"
+opt.completeopt = "menuone,noselect"
+opt.termguicolors = true
+
+vim.g.netrw_banner = 0
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
