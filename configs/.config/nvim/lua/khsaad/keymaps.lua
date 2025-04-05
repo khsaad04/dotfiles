3912df05e5344de27efd178fcfdedffaece4c407
khsaad04 <khsaad0404@gmail.com>
2025-04-05 20:59:35 +0600

    feat(nvim): Remove some useless keymaps

--- configs/.config/nvim/lua/khsaad/keymaps.lua
+++ configs/.config/nvim/lua/khsaad/keymaps.lua
@@ -1,9 +1,6 @@
 local map = vim.keymap.set
 
-map("", "<Space>", "<Nop>")
-
 vim.g.mapleader = " "
-vim.g.maplocalleader = " "
 
 -- Better window navigation
 map("n", "<C-h>", "<C-w>h")
@@ -12,19 +9,13 @@ map("n", "<C-k>", "<C-w>k")
 map("n", "<C-l>", "<C-w>l")
 
 -- Clear highlights on search
-map({ "n", "v" }, "<leader>h", "<cmd>nohlsearch<CR>", { desc = "Clear highlights on search" })
+map("n", "<leader>h", "<cmd>nohlsearch<CR>", { desc = "Clear highlights on search" })
 
 -- Stay in indent mode while indenting
 map("v", "<", "<gv")
 map("v", ">", ">gv")
 
 -- Move Lines
-map("n", "<A-j>", "<cmd>m .+1<cr>==")
-map("n", "<A-k>", "<cmd>m .-2<cr>==")
-
-map("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi")
-map("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi")
-
 map("v", "<A-j>", ":m '>+1<cr>gv=gv")
 map("v", "<A-k>", ":m '<-2<cr>gv=gv")
 
