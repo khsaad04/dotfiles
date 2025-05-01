9245829dd48db2de85d6240346dda07f8ece9722
khsaad04 <khsaad0404@gmail.com>
2025-05-01 11:12:32 +0600

    Remove redundant nix pkgs

--- packages/default.nix
+++ packages/default.nix
@@ -1,6 +1,4 @@
 { pkgs, ... }:
 {
   formatter = pkgs.callPackage ./formatter.nix { };
-  changevolume = pkgs.callPackage ./changevolume.nix { };
-  powermenu = pkgs.callPackage ./powermenu.nix { };
 }
