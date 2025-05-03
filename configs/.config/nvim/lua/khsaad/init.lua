81a6d27a2165a10505427586b697c0fe42f16ca2
khsaad04 <khsaad0404@gmail.com>
2025-05-03 16:19:03 +0600

    [nvim] let's try out blink.cmp one more time

--- configs/.config/nvim/lua/khsaad/init.lua
+++ configs/.config/nvim/lua/khsaad/init.lua
@@ -66,61 +66,11 @@ end)
 
 later(function()
     add({
-        source = "hrsh7th/nvim-cmp",
-        depends = {
-            "hrsh7th/cmp-nvim-lsp",
-            "hrsh7th/cmp-buffer",
-            "hrsh7th/cmp-path",
-            "hrsh7th/cmp-cmdline",
-            "L3MON4D3/LuaSnip",
-            "saadparwaiz1/cmp_luasnip",
-            "rafamadriz/friendly-snippets",
-        },
+        source = "saghen/blink.cmp",
+        depends = { "rafamadriz/friendly-snippets" },
+        version = "v1.1.1"
     })
-
-    -- LuaSnip
-    local luasnip = require("luasnip")
-
-    require("luasnip.loaders.from_vscode").lazy_load()
-    luasnip.config.setup({})
-
-    -- nvim-cmp
-    local cmp = require("cmp")
-
-    cmp.setup({
-        snippet = {
-            expand = function(args)
-                luasnip.lsp_expand(args.body)
-            end,
-        },
-        mapping = cmp.mapping.preset.insert({
-            ["<CR>"] = cmp.mapping.confirm({ select = true }),
-            ["<Tab>"] = cmp.mapping.select_next_item(),
-            ["<S-Tab>"] = cmp.mapping.select_prev_item(),
-        }),
-        sources = cmp.config.sources({
-            { name = "nvim_lsp" },
-            { name = "buffer" },
-            { name = "path" },
-            { name = "luasnip" },
-        }),
-    })
-
-    cmp.setup.cmdline({ "/", "?" }, {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = {
-            { name = "buffer" },
-        },
-    })
-
-    cmp.setup.cmdline(":", {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = cmp.config.sources({
-            { name = "path" },
-            { name = "cmdline" },
-        }),
-        matching = { disallow_symbol_nonprefix_matching = false },
-    })
+    require("blink.cmp").setup()
 end)
 
 later(function()
