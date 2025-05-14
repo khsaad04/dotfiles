53a59eb0d38f65f987d45382d753344166c7f88c
khsaad04 <khsaad0404@gmail.com>
2025-05-14 14:33:47 +0600

    [nvim] configure lsps like nvim-lspconfig default config

--- configs/.config/nvim/lsp/luals.lua
+++ configs/.config/nvim/lsp/luals.lua
@@ -1,7 +1,15 @@
 return {
     cmd = { "lua-language-server" },
     filetypes = { "lua" },
-    root_markers = { ".stylua.toml" },
+    root_markers = {
+        ".luarc.json",
+        ".luarc.jsonc",
+        ".luacheckrc",
+        ".stylua.toml",
+        "stylua.toml",
+        "selene.toml",
+        "selene.yml",
+    },
     settings = {
         Lua = {
             diagnostics = {
