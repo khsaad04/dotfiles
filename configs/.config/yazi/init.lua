a89deb0c5785d0e61bc4543a5165f0676c3b5d06
khsaad04 <khsaad0404@gmail.com>
2025-05-18 16:07:55 +0600

    [yazi] fix init.lua

--- configs/.config/yazi/init.lua
+++ configs/.config/yazi/init.lua
@@ -1,61 +1,59 @@
+-- Full border https://yazi-rs.github.io/docs/tips#full-border
 local type = opts and opts.type or ui.Border.ROUNDED
 local old_build = Tab.build
 
 Tab.build = function(self, ...)
-	local bar = function(c, x, y)
-		if x <= 0 or x == self._area.w - 1 then
-			return ui.Bar(ui.Bar.TOP)
-		end
+    local bar = function(c, x, y)
+        if x <= 0 or x == self._area.w - 1 or th.mgr.border_symbol ~= "│" then
+            return ui.Bar(ui.Bar.TOP)
+        end
 
-		return ui.Bar(ui.Bar.TOP)
-			:area(ui.Rect({
-				x = x,
-				y = math.max(0, y),
-				w = ya.clamp(0, self._area.w - x, 1),
-				h = math.min(1, self._area.h),
-			}))
-			:symbol(c)
-	end
+        return ui.Bar(ui.Bar.TOP)
+            :area(ui.Rect({
+                x = x,
+                y = math.max(0, y),
+                w = ya.clamp(0, self._area.w - x, 1),
+                h = math.min(1, self._area.h),
+            }))
+            :symbol(c)
+    end
 
-	local c = self._chunks
-	self._chunks = {
-		c[1]:padding(ui.Padding.y(1)),
-		c[2]:padding(ui.Padding(c[1].w > 0 and 0 or 1, c[3].w > 0 and 0 or 1, 1, 1)),
-		c[3]:padding(ui.Padding.y(1)),
-	}
+    local c = self._chunks
+    self._chunks = {
+        c[1]:pad(ui.Pad.y(1)),
+        c[2]:pad(ui.Pad(1, c[3].w > 0 and 0 or 1, 1, c[1].w > 0 and 0 or 1)),
+        c[3]:pad(ui.Pad.y(1)),
+    }
 
-	local style = THEME.manager.border_style
-	self._base = ya.list_merge(self._base or {}, {
-		ui.Border(ui.Border.ALL):area(self._area):type(type):style(style),
-		ui.Bar(ui.Bar.RIGHT):area(self._chunks[1]):style(style),
-		ui.Bar(ui.Bar.LEFT):area(self._chunks[3]):style(style),
+    local style = th.mgr.border_style
+    self._base = ya.list_merge(self._base or {}, {
+        ui.Border(ui.Border.ALL):area(self._area):type(type):style(style),
+        ui.Bar(ui.Bar.RIGHT):area(self._chunks[1]):style(style),
+        ui.Bar(ui.Bar.LEFT):area(self._chunks[3]):style(style),
 
-		bar("┬", c[1].right - 1, c[1].y),
-		bar("┴", c[1].right - 1, c[1].bottom - 1),
-		bar("┬", c[2].right, c[2].y),
-		bar("┴", c[2].right, c[2].bottom - 1),
-	})
+        bar("┬", c[1].right - 1, c[1].y),
+        bar("┴", c[1].right - 1, c[1].bottom - 1),
+        bar("┬", c[2].right, c[2].y),
+        bar("┴", c[2].right, c[2].bottom - 1),
+    })
 
-	old_build(self, ...)
+    old_build(self, ...)
 end
 
-function Status:name()
-	local h = self._current.hovered
-	if not h then
-		return ""
-	end
+-- Show symlink in status bar https://yazi-rs.github.io/docs/tips#symlink-in-status
+Status:children_add(function(self)
+    local h = self._current.hovered
+    if h and h.link_to then
+        return " -> " .. tostring(h.link_to)
+    else
+        return ""
+    end
+end, 3300, Status.LEFT)
 
-	local linked = ""
-	if h.link_to ~= nil then
-		linked = " -> " .. tostring(h.link_to)
-	end
-
-	return " " .. h.name:gsub("\r", "?", 1) .. linked
-end
-
+-- Show username and hostname in header https://yazi-rs.github.io/docs/tips#username-hostname-in-header
 Header:children_add(function()
-	if ya.target_family() ~= "unix" then
-		return ""
-	end
-	return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg("blue")
+    if ya.target_family() ~= "unix" then
+        return ""
+    end
+    return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg("blue")
 end, 500, Header.LEFT)
