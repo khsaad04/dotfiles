6c90ad6282a8c6c9a6c918be40067fc69534cd26
khsaad04 <khsaad0404@gmail.com>
2025-05-21 18:05:56 +0600

    [fish] remove aliases to get used to typing out entire commands

--- configs/.config/fish/config.fish
+++ configs/.config/fish/config.fish
@@ -3,14 +3,6 @@ set -gx RIPGREP_CONFIG_PATH ~/.config/ripgreprc
 set -gx EDITOR nvim
 set -gx VISUAL nvim
 
-# Aliases
-alias l 'ls -lah'
-alias t 'tmux a || tmux new -s main'
-alias y yazi
-alias vi nvim
-alias vim nvim
-alias vimdiff "nvim -d"
-
 # Path
 fish_add_path -aP ~/.cargo/bin
 fish_add_path -aP ~/bin
