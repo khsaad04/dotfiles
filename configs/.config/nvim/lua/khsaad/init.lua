f56ad99a29eb2bf38bc8e422300847009ed9cf6e
khsaad04 <khsaad0404@gmail.com>
2025-07-29 17:09:07 +0600

    [nvim] prefer git while downloading tree-sitter parsers

--- configs/.config/nvim/lua/khsaad/init.lua
+++ configs/.config/nvim/lua/khsaad/init.lua
@@ -38,6 +38,7 @@ later(function()
             end,
         },
     })
+    require("nvim-treesitter.install").prefer_git = true
     require("nvim-treesitter.configs").setup({
         highlight = { enable = true },
         indent = { enable = true },
