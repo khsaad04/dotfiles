1b70b64edd30a3099f19aad2ff0605e5dd2f77cc
khsaad04 <khsaad0404@gmail.com>
2025-08-06 23:10:37 +0600

    [nvim] remove voodoo

--- configs/.config/nvim/init.lua
+++ configs/.config/nvim/init.lua
@@ -2,26 +2,21 @@
 --
 vim.opt.number         = true
 vim.opt.relativenumber = true
-
 vim.opt.softtabstop    = 4
 vim.opt.shiftwidth     = 4
 vim.opt.expandtab      = true
 vim.opt.smartindent    = true
-
 vim.opt.splitbelow     = true
 vim.opt.splitright     = true
 vim.opt.wrap           = false
 vim.opt.scrolloff      = 999
 vim.opt.virtualedit    = "block"
 vim.opt.signcolumn     = "yes"
-
+vim.opt.hlsearch       = false
 vim.opt.clipboard      = "unnamedplus"
 vim.opt.undofile       = true
 vim.opt.swapfile       = false
-
 vim.opt.ignorecase     = true
-vim.opt.smartcase      = true
-vim.opt.inccommand     = "split"
 
 -- [[ Colorscheme ]] --
 --
@@ -34,15 +29,9 @@ vim.keymap.set("n", "<Space>", "<Nop>", { silent = true })
 vim.g.mapleader      = " "
 vim.g.maplocalleader = " "
 
-vim.keymap.set("n", "<leader>h", "<cmd>nohlsearch<CR>", { desc = "Clear highlights on search" })
-
 -- Stay in visual mode while indenting
 vim.keymap.set("v", "<", "<gv")
 vim.keymap.set("v", ">", ">gv")
-
-vim.keymap.set("n", "<space>e",   vim.diagnostic.open_float,                                             { desc = "Open diagnostic float"         })
-vim.keymap.set("n", "<leader>q",  vim.diagnostic.setloclist,                                             { desc = "Open diagnostic quickfix list" })
-vim.keymap.set("n", "<leader>td", "<cmd>lua vim.diagnostic.enable(not vim.diagnostic.is_enabled())<CR>", { desc = "Toggle diagnostics"            })
 
 vim.keymap.set("n", "yo", function()
     local pos  = vim.api.nvim_win_get_cursor(0)
@@ -76,38 +65,11 @@ require("mini.deps").setup({ path = { package = path_package } })
 
 local add, now, later = MiniDeps.add, MiniDeps.now, MiniDeps.later
 
-now(function()
-    -- tree-sitter highlights must load asap
-    add({
-        source = "nvim-treesitter/nvim-treesitter",
-        hooks  = {
-            post_checkout = function()
-                vim.cmd("TSUpdate")
-            end,
-        },
-    })
-    require("nvim-treesitter.install").prefer_git = true
-    require("nvim-treesitter.configs").setup({
-        highlight = { enable = true },
-        indent    = { enable = true },
-    })
-end)
-
 later(function()
-    -- we can lazy load rest of these
-    --
     -- vim-visual-multi
     vim.g.VM_add_cursor_at_pos_no_mappings = 1
     add({ source = "mg979/vim-visual-multi" })
-    vim.keymap.set("n", "<leader>vmm", "<Plug>(VM-Toggle-Mappings)", { desc = "Toggle [V]isual [M]ulti [M]appings" })
-
-    -- blink.cmp
-    add({
-        source = "saghen/blink.cmp",
-        depends = { "rafamadriz/friendly-snippets" },
-        checkout = "v1.5.1",
-    })
-    require("blink.cmp").setup()
+    vim.keymap.set("n", "<leader>vmm", "<Plug>(VM-Toggle-Mappings)", { desc = "Toggle visual multi mappings" })
 
     -- mini.icons
     require("mini.icons").setup()
@@ -123,22 +85,22 @@ later(function()
         if not MiniFiles.close() then
             MiniFiles.open(vim.api.nvim_buf_get_name(0))
         end
-    end, { desc = "File [E]xplorer" })
+    end, { desc = "Toggle file explorer" })
 
     -- mini.pick
     require("mini.pick").setup()
 
     vim.keymap.set("n", "<leader>sf", function()
         MiniPick.builtin.files()
-    end, { desc = "[S]earch [F]iles" })
+    end, { desc = "Search files" })
 
     vim.keymap.set("n", "<leader>sg", function()
         MiniPick.builtin.grep_live()
-    end, { desc = "[S]earch by [G]rep" })
+    end, { desc = "Search by grep" })
 
     vim.keymap.set("n", "<leader>sh", function()
         MiniPick.builtin.help()
-    end, { desc = "[S]earch [H]elp" })
+    end, { desc = "Search help" })
 
     -- gitsigns
     add({ source = "lewis6991/gitsigns.nvim" })
@@ -153,8 +115,6 @@ later(function()
     vim.lsp.enable({
         "clangd",
         "lua_ls",
-        "pylsp",
-        "ruff",
         "rust_analyzer",
     })
 
