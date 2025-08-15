c4a088ce35bc3e0d0cc3f27344f86abc0d3d5537
khsaad04 <khsaad0404@gmail.com>
2025-08-15 23:08:02 +0600

    [nvim] remove useless option

--- configs/.config/nvim/init.lua
+++ configs/.config/nvim/init.lua
@@ -5,7 +5,6 @@ vim.opt.relativenumber = true
 vim.opt.softtabstop    = 4
 vim.opt.shiftwidth     = 4
 vim.opt.expandtab      = true
-vim.opt.smartindent    = true
 vim.opt.splitbelow     = true
 vim.opt.splitright     = true
 vim.opt.wrap           = false
