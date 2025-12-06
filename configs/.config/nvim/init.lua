cbcef0f61ba35aa8dbb5e7331808c15f27f9b0c4
khsaad04 <khsaad0404@gmail.com>
2025-12-06 20:52:07 +0600

    [nvim] Replace builtin completion with blink.cmp

--- configs/.config/nvim/init.lua
+++ configs/.config/nvim/init.lua
@@ -135,14 +135,12 @@ later(function()
         },
     })
 
-    vim.api.nvim_create_autocmd("LspAttach", {
-        callback = function(args)
-            local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-            if client:supports_method("textDocument/completion") then
-                vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
-            end
-        end,
+    add({
+        source = "saghen/blink.cmp",
+        depends = { "rafamadriz/friendly-snippets" },
+        checkout = "v1.8.0",
     })
+    require("blink.cmp").setup()
 
     vim.diagnostic.enable(false) -- disable diagnostics by default
 end)
